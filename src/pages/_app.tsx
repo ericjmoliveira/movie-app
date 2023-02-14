a8e0d89ebeeaef07c7fe85fbee0d69efe61fc192
Eric Oliveira <ericjmoliveira@gmail.com>
2023-02-14 13:16:52 -0300

    Set up React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useRef } from 'react';
+import { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = useRef(new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient.current}>
+      <Hydrate>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
