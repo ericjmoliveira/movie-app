e36e3c83fb4e01f676cdb96cacd1ac00d25b0486
Eric Oliveira <ericjmoliveira@gmail.com>
2023-02-16 13:00:29 -0300

    Create movie data interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -6,18 +6,46 @@ export interface ResponseData {
 }
 
 export interface Results {
-  poster_path?: string;
   adult: boolean;
-  overview: string;
-  release_date: string;
+  backdrop_path?: string;
   genre_ids: number[];
   id: number;
-  original_title: string;
   original_language: string;
-  title: string;
-  backdrop_path?: string;
+  original_title: string;
+  overview: string;
   popularity: number;
-  vote_count: number;
+  poster_path?: string;
+  release_date: string;
+  title: string;
   video: boolean;
   vote_average: number;
+  vote_count: number;
+}
+
+export interface MovieData {
+  adult: boolean;
+  backdrop_path?: string;
+  belongs_to_collection?: object;
+  budget: number;
+  genres: [{ id: number; name: string }];
+  homepage?: string;
+  id: number;
+  imdb_id?: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path?: string;
+  production_companies: [{ name: string; id: number; logo_path?: string; origin_country: string }];
+  production_countries: [{ iso_3166_1: string; name: string }];
+  release_date: string;
+  revenue: number;
+  runtime?: number;
+  spoken_languages: [{ iso_639_1: string; name: string }];
+  status: string;
+  tagline?: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
